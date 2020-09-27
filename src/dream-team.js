553e680072addaf3058b06f9743894066e4e475d
AleksTret <Aleks.Tret@gmail.com>
2020-09-27 23:58:02 +0300

    feat: add createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members) || members.length === 0){
+    return false;
+  }
+   
+  let result = members.filter(item => {
+    return typeof item === "string" && item.length !== 0;
+  })
+
+  return result.map(item => item.trim().toUpperCase()).sort().map(item => item[0]).join("");
 };
