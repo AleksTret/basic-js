8d41a7444209e631ad2d28d3e05b3cac5aa2d7d0
AleksTret <Aleks.Tret@gmail.com>
2020-09-29 22:44:32 +0300

    feat: add repeater()

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+
+  let result;  
+  let addition;
   
+  if(options.additionRepeatTimes){
+			addition = (options.addition + (options.additionSeparator || "|"))
+      	.repeat(options.additionRepeatTimes - 1) + options.addition;
+    
+  }
+  else{ 
+    addition = options.addition || "";
+  }
+  
+  if (options.repeatTimes){
+      result = (str + addition + (options.separator || '+'))
+      	.repeat(options.repeatTimes - 1) + str + addition;
+  }
+  else{
+  	result = str + addition;
+  }
+
+  
+	return result;
+}
+
